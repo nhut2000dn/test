4b28157d728f3f52fe42e2e5a36769852284c5ac
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-10 02:18:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from json import dumps
 app = Flask(__name__)
 api = Api(app)
 
+class Employees(Resource):
+    def get(self, text):
+        return response(text)
+
 @app.route('/')
 def index():
     return "<h1>Welcome to CodingX</h1>"
+
+api.add_resource(Employees, '/send/<text>') 
 
 if __name__ == '__main__':
      app.run()
