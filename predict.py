93a853380195f7da2388ab1c1739febbc679e341
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-10 03:29:24 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -222,7 +222,7 @@ def classify(sentence):
     return return_list
 
 def response(sentence, show_details=False):
-    return regex.findall(r'(?i)\b\p{L}+\b', intents['intents'][0]['tag'])
+    return (words)
 #     results = classify(sentence)
 #     if results:
 #         while results:
