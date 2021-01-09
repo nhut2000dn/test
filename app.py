0adb59059770326e6008d1221eec83203d2ae636
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-09 23:22:12 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)   
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to CodingX</h1>"
+
+    
+if __name__ == '__main__':
+     app.run()
