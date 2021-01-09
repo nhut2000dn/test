ba2e446bf74d3175b286e4ae3dba0a1d641c9aa9
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-10 02:15:59 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -172,68 +172,67 @@ import COVID19Py
 
 import pickle
 data = pickle.load( open( "./models/training_data", "rb" ) )
-# words = data['words']
-# classes = data['classes']
-# train_x = data['train_x']
-# train_y = data['train_y']
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
 
 
-# net = tflearn.input_data(shape=[None, len(train_x[0])])
-# net = tflearn.fully_connected(net, 8)
-# net = tflearn.fully_connected(net, 8)
-# net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-# net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
 
-# model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# covid19 = COVID19Py.COVID19()
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+covid19 = COVID19Py.COVID19()
 
 
-# def clean_up_sentence(sentence):
-#     sentence_words = nltk.word_tokenize(sentence)
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
 
-#     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-#     return sentence_words
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
 
-# def bow(sentence, words, show_details=False):
-#     sentence_words = clean_up_sentence(sentence)
-#     bag = [0]*len(words)  
-#     for s in sentence_words:
-#         for i,w in enumerate(words):
-#             if w == s: 
-#                 bag[i] = 1
-#                 if show_details:
-#                     print ("found in bag: %s" % w)
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
 
-#     return(np.array(bag))
+    return(np.array(bag))
 
-# # load model
-# model.load('C:/xampp/htdocs/ChatBot-Covid/Back-end/models/model.tflearn')
-# context = {}
+# load model
+model.load('./models/model.tflearn')
+context = {}
 
-# ERROR_THRESHOLD = 0.25
-# def classify(sentence):
-#     results = model.predict([bow(sentence, words)])[0]
-#     results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
-#     results.sort(key=lambda x: x[1], reverse=True)
-#     return_list = []
-#     for r in results:
-#         return_list.append((classes[r[0]], r[1]))
-#     print(return_list)
-#     return return_list
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    results = model.predict([bow(sentence, words)])[0]
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    print(return_list)
+    return return_list
 
 def response(sentence, show_details=False):
-  return 0
-#     results = classify(sentence)
-#     if results:
-#         while results:
-#             for i in intents['intents']:
-#                 if i['tag'] == results[0][0]:
-#                   if results[0][0] in "numberCovidCase":
-#                     NumberComfirmed = " Số người mắc bệnh: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["confirmed"])
-#                     NumberDeath = ", Số người chết: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["deaths"])
-#                     print((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
-#                     return ((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
-#                   else:
-#                     return (random.choice(i['responses']))
+    results = classify(sentence)
+    if results:
+        while results:
+            for i in intents['intents']:
+                if i['tag'] == results[0][0]:
+                  if results[0][0] in "numberCovidCase":
+                    NumberComfirmed = " Số người mắc bệnh: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["confirmed"])
+                    NumberDeath = ", Số người chết: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["deaths"])
+                    print((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
+                    return ((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
+                  else:
+                    return (random.choice(i['responses']))
                   
-#             results.pop(0)
+            results.pop(0)
