707bad9a97a9e264dfc825d0c70e8a2e18bd1baf
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-17 02:41:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,10 @@ def index():
 
 api.add_resource(Employees, '/send/<text>') 
 
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 if __name__ == '__main__':
      app.run()
