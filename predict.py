24b15192470e2b780f3ffaa68036423a7c74722b
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-18 07:24:54 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -191,10 +191,10 @@ covid19 = COVID19Py.COVID19()
 
 
 def clean_up_sentence(sentence):
-#     sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = nltk.word_tokenize(sentence)
 
-#     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
 
 def bow(sentence, words, show_details=False):
     sentence_words = clean_up_sentence(sentence)
