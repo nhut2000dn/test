f30b46912f9ab7ecb01ea9a83901cc34ffffef31
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-18 07:09:41 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -223,17 +223,18 @@ def classify(sentence):
     print(return_list)
 
 def response(sentence, show_details=False):
-  results = classify(sentence)
-  if results:
-      while results:
-          for i in intents['intents']:
-              if i['tag'] == results[0][0]:
-                if results[0][0] in "numberCovidCase":
-                  NumberComfirmed = " Số người mắc bệnh: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["confirmed"])
-                  NumberDeath = ", Số người chết: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["deaths"])
-                  print((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
-                  return ((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
-                else:
-                  return 'hi'
+  return (clean_up_sentence(sentence))
+#   results = classify(sentence)
+#   if results:
+#       while results:
+#           for i in intents['intents']:
+#               if i['tag'] == results[0][0]:
+#                 if results[0][0] in "numberCovidCase":
+#                   NumberComfirmed = " Số người mắc bệnh: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["confirmed"])
+#                   NumberDeath = ", Số người chết: " + str((covid19.getLocationByCountryCode("VN"))[0]["latest"]["deaths"])
+#                   print((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
+#                   return ((random.choice(i['responses'])) + NumberComfirmed + NumberDeath)
+#                 else:
+#                   return 'hi'
 
-          results.pop(0)
+#           results.pop(0)
