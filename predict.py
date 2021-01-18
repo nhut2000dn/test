f403c71fbc68959d6910a6462fa4c78e6b1937cb
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-18 07:18:26 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -194,7 +194,7 @@ def clean_up_sentence(sentence):
 #     sentence_words = nltk.word_tokenize(sentence)
 
 #     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
+    return sentence
 
 def bow(sentence, words, show_details=False):
     sentence_words = clean_up_sentence(sentence)
